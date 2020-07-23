0e88e5e13bfcf884c62e8c52b9e01253af8c890d
zlataovce <34477304+zlataovce@users.noreply.github.com>
2020-07-23 17:59:12 +0200

    functioning version

--- hazedumper.py
+++ hazedumper.py
@@ -2,6 +2,16 @@ from urllib import urlretrieve
 
 def dump():
     urlretrieve("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.hpp", "csgo.py")
-    with open("csgo.py", "r+") as hazedumper:
-        for line in hazedumper:
-            if "constexpr ::std::int64_t timestamp" in line.rstrip():
+    with open("csgo.py", "r") as hazedumper:
+        data = hazedumper.readlines()
+    for line in data:
+        if "// " in line:
+            if "UTC" in line:
+                line.replace("//", "# hazedumper timestamp")
+        elif "constexpr ::std::ptrdiff_t " in line:
+            line.replace("constexpr ::std::ptrdiff_t ", "")
+            line.replace(";", "")
+        else:
+            line = ''
+    with open("csgo.py", "w") as hazedumper:
+        hazedumper.writelines(data)
