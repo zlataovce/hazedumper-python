6b329fae4c6eb9d12edbcd42cac7180d8b3b691d
zlataovce <34477304+zlataovce@users.noreply.github.com>
2020-07-24 13:47:26 +0200

    fully working version

--- hazedumper.py
+++ hazedumper.py
@@ -1,6 +1,12 @@
 from urllib.request import urlretrieve
+import os
 
 def dump(debug=False):
+    if os.path.exists("csgo.hpp"):
+        os.remove("csgo.hpp")
+    
+    if os.path.exists("offsets.py"):
+        os.remove("offsets.py")
     urlretrieve("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.hpp", "csgo.hpp")
     if debug == True:
         print("Got file!")
@@ -10,41 +16,43 @@ def dump(debug=False):
         data = hazedumper.readlines()
         if debug == True:
             print("Read lines!")
-        for line in data:
+    offsets = open("offsets.py", "a")
+    for line in data:
+        if debug == True:
+            print("Going to iterate!")
+        if "// " in line:
             if debug == True:
-                print("Going to iterate!")
-            if "// " in line.rstrip():
+                print("Identified C++ comment!")
+            if "UTC" in line:
                 if debug == True:
-                    print("Identified C++ comment!")
-                if "UTC" in line.rstrip():
-                    if debug == True:
-                        print("Identified timestamp!")
-                    line = line.replace("//", "# hazedumper timestamp")
-                    if debug == True:
-                        print("Wrote timestamp!")
-                else:
-                    if debug == True:
-                        print("Identified comment, but no timestamp!")
-                    line = ''
-                    if debug == True:
-                        print("Wrote empty line!")
-            elif "constexpr ::std::ptrdiff_t " in line.rstrip():
+                    print("Identified timestamp!")
+                line = line.replace("//", "# hazedumper timestamp")
+                offsets.write(line)
                 if debug == True:
-                    print("Identified offset!")
-                line = line.replace("constexpr ::std::ptrdiff_t ", "")
-                line = line.replace(";", ")")
-                line = line.replace("= ", "= (")
-                if debug == True:
-                    print("Wrote offset to pythonic form!")
+                    print("Wrote timestamp!")
             else:
                 if debug == True:
-                    print("Not a valid offset!")
+                    print("Identified comment, but no timestamp!")
                 line = ''
+                offsets.write(line)
                 if debug == True:
-                    print("Wrote an empty line!")
-        with open("offsets.py", "w") as offsetwrite:
+                    print("Wrote empty line!")
+        elif "constexpr ::std::ptrdiff_t " in line:
             if debug == True:
-                print("Wrote data to python file!")
-            offsetwrite.writelines(data)
+                print("Identified offset!")
+            line = line.replace("constexpr ::std::ptrdiff_t ", "")
+            line = line.replace(";", ")")
+            line = line.replace("= ", "= (")
+            offsets.write(line)
+            if debug == True:
+                print("Wrote offset to pythonic form!")
+        else:
+            if debug == True:
+                print("Not a valid offset!")
+            line = ''
+            offsets.write(line)
+            if debug == True:
+                print("Wrote an empty line!")
+    offsets.close()
 
 dump(debug=False)
