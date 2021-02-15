708602caa1929ae15d61e69dcc26ec97043ed811
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-02-15 12:16:18 +0100

    converting to int fixed

--- hazedumper.py
+++ hazedumper.py
@@ -13,6 +13,6 @@ def dump():
         if "constexpr ::std::ptrdiff_t " in line:
             line = line.replace("constexpr ::std::ptrdiff_t ", "")
             item_list = line.replace(";", "").rstrip().split(" = ")
-            offsets[item_list[0]] = item_list[1]
+            offsets[item_list[0]] = int(item_list[1], 16)
     os.remove("csgo.hpp")
     return offsets
